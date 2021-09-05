c0a47273c7e1e1a28e4bcf2b1fe2432c1265c3ce
Connie Tsai <connieatsai@gmail.com>
2021-09-04 18:10:00 -0700

    Serve mail data at /mail endpoint

--- dashboard/server/index.js
+++ dashboard/server/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+const mailData = require('../client/src/api/mail.js');
 // app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.static(path.join(__dirname, '../client/public')));
+app.use(express.static(path.join(__dirname, '../client/src/api')));
 
 app.get('/mail', (req, res) => {
-  res.send('requested mail');
+
+  res.send(mailData);
 })
 
 app.listen(port, () => {
