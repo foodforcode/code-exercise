74fad1b20106b4f84263f9ac1a01793aac6906a9
Connie Tsai <connieatsai@gmail.com>
2021-09-08 15:01:02 -0700

    Update files for Heroku deployment

--- dashboard/server/index.js
+++ dashboard/server/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mailData = require('../client/src/api/mail.js');
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.static(path.join(__dirname, '../client/public')));
