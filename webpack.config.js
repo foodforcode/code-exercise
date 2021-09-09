ca27786a50584ff705314d5f494629e8c9925d8b
Connie Tsai <connieatsai@gmail.com>
2021-09-08 20:45:51 -0700

    Add postbuild scripts for Heroku deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './dashboard/client/src/index.js',
   output: {
     path: path.resolve(__dirname, 'dashboard/client/dist'),
